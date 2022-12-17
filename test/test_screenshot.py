b49627622cabedcf752f95d94f3c3cde54000e5b
py85329140 <3293406747@qq.com>
2022-12-17 22:13:40 +0800

    新增部分测试代码，修复了已知的问题

---
+++ test/test_screenshot.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+class TestBaidu:
+
+	@pytest.fixture(scope="class", autouse=True)
+	def goto(self, page):
+		page.get("https://www.baidu.com")
+
+	def test_elem_save_screenshot(self, page):
+		location = By.ID, "su"
+		page.elem_save_screenshot(location)
